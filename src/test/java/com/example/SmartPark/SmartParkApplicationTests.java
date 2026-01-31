b66e637309c2a8b66578cec6cb89ca7aba5265d5
makifranco16 <makifranco68@gmail.com>
2026-02-01 01:36:38 +0800

    Spring

---
+++ src/test/java/com/example/SmartPark/SmartParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
